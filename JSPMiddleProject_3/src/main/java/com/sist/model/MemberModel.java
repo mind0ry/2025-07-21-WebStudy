c346006ba06a3fbbee25ffb9c1ac12d67a8fce27
KIM MINSEOK <mindory144@gmail.com>
2025-10-28 17:47:55 +0900

    2025-10-28 zipcode 연습

---
+++ JSPMiddleProject_3/src/main/java/com/sist/model/MemberModel.java
@@ -0,0 +1,23 @@
+package com.sist.model;
+import java.util.*;
+
+import com.sist.controller.Controller;
+import com.sist.controller.RequestMapping;
+import com.sist.dao.*;
+import com.sist.vo.*;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@Controller
+public class MemberModel {
+	@RequestMapping("member/post.do")
+	public String member_post(HttpServletRequest request,HttpServletResponse response) {
+		// 요청값 받기
+		String dong=request.getParameter("dong");
+		// 요청 처리
+		List<ZipcodeVO> list=MemberDAO.postFindData(dong);
+		// 출력할 결과값 전송 => JSON/request
+		request.setAttribute("list", list);
+		return "../member/post.jsp";
+	}
+}
